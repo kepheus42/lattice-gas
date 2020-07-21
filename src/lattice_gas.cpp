c7ff23ad1c5b3f78462f3b68744790a17ca44d74
kepheus42 <kepheus42@gmail.com>
2020-07-21 10:16:24 +0200

    removing references to 3x3

--- src/lattice_gas.cpp
+++ src/lattice_gas.cpp
@@ -18,10 +18,6 @@
 #endif
 // To add debugging messages, use D(std::cerr << "Debugging message 1 2 3!" << std::endl; )
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
 int main(int ac, char** av){
         set_rng_seed(0);
         printf("START!\n");
@@ -35,26 +31,14 @@ int main(int ac, char** av){
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         int number_of_tracers_1x1 = atoi(av[5]);
         int number_of_tracers_2x2 = atoi(av[6]);
-<<<<<<< HEAD
-=======
-        int number_of_tracers_3x3 = 0; //atoi(av[5]);
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         int wtd_max = atoi(av[7]);
         int wtd_res = atoi(av[8]);
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-<<<<<<< HEAD
         double step_rate_1x1 = atoi(av[9]);
         double step_rate_2x2 = atoi(av[10]);
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         int number_of_lattices = atoi(av[11]);
-=======
-        double step_rate_1x1 = 1;
-        double step_rate_2x2 = 1/2;
-        double step_rate_3x3 = 1/3; // atof(av[11]);
-        // - - - - - - - - - - - - - - - - - - - - - - - - -
-        int number_of_lattices = atoi(av[9]);
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         printf("X:%4d Y:%4d 1:%6d 2:%6d T:%8d W:%6d L:%10d\n",grid_size_x,grid_size_y,number_of_tracers_1x1,number_of_tracers_2x2,number_of_timesteps,number_of_timesteps_warmup,number_of_lattices);
         // - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -66,19 +50,16 @@ int main(int ac, char** av){
         std::string output_file_name_rate_1x1;
         std::string output_file_name_rate_2x2;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-<<<<<<< HEAD
         std::string output_file_name_sublattice_conc_1x1;
         std::string output_file_name_sublattice_conc_2x2;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-=======
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
         std::string output_file_name_wtd_1x1;
         std::string output_file_name_wtd_2x2;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         std::string output_file_name_corr_1x1;
         std::string output_file_name_corr_2x2;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-        output_file_name_base_string = "X_"+std::to_string(grid_size_x)+"_Y_"+std::to_string(grid_size_y)+"_N1_"+std::to_string(number_of_tracers_1x1)+"_N2_"+std::to_string(number_of_tracers_2x2)+"_N3_"+std::to_string(number_of_tracers_3x3)+"_T_"+std::to_string(number_of_timesteps)+"_W_"+std::to_string(number_of_timesteps_warmup)+".bin";
+        output_file_name_base_string = "X_"+std::to_string(grid_size_x)+"_Y_"+std::to_string(grid_size_y)+"_N1_"+std::to_string(number_of_tracers_1x1)+"_N2_"+std::to_string(number_of_tracers_2x2)+"_T_"+std::to_string(number_of_timesteps)+"_W_"+std::to_string(number_of_timesteps_warmup)+".bin";
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         output_file_name_msd_1x1 = "msd_1x1_"+output_file_name_base_string;
         output_file_name_msd_2x2 = "msd_2x2_"+output_file_name_base_string;
@@ -86,15 +67,12 @@ int main(int ac, char** av){
         output_file_name_rate_1x1 = "rate_1x1_"+output_file_name_base_string;
         output_file_name_rate_2x2 = "rate_2x2_"+output_file_name_base_string;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-<<<<<<< HEAD
         output_file_name_sublattice_conc_1x1 = "sub_c_1x1_"+output_file_name_base_string;
         output_file_name_sublattice_conc_2x2 = "sub_c_2x2_"+output_file_name_base_string;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
         output_file_name_neighbor_count_1x1 = "";
         output_file_name_neighbor_count_2x2 = "";
         // - - - - - - - - - - - - - - - - - - - - - - - - -
-=======
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
         output_file_name_wtd_1x1 = "wtd_1x1_"+output_file_name_base_string;
         output_file_name_wtd_2x2 = "wtd_2x2_"+output_file_name_base_string;
         // - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -113,10 +91,10 @@ int main(int ac, char** av){
                                         number_of_timesteps_warmup,
                                         number_of_tracers_1x1,
                                         number_of_tracers_2x2,
-                                        number_of_tracers_3x3,
+                                        0,
                                         step_rate_1x1,
                                         step_rate_2x2,
-                                        step_rate_3x3,
+                                        1.0,
                                         wtd_max,
                                         wtd_res);
         D(std::cerr << "Done!" << std::endl);
@@ -147,11 +125,8 @@ int main(int ac, char** av){
                                output_file_name_msd_1x1);
                 vector_to_file(wrapper->get_result_rate_1x1(),
                                output_file_name_rate_1x1);
-<<<<<<< HEAD
                 vector_to_file(wrapper->get_result_sublattice_conc_1x1(),
                                output_file_name_sublattice_conc_1x1);
-=======
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
                 vector_to_file(wrapper->get_result_wtd_1x1(),
                                output_file_name_wtd_1x1);
                 vector_to_file(wrapper->get_result_correlations_1x1(),
@@ -164,20 +139,14 @@ int main(int ac, char** av){
                                output_file_name_msd_2x2);
                 vector_to_file(wrapper->get_result_rate_2x2(),
                                output_file_name_rate_2x2);
-<<<<<<< HEAD
                 vector_to_file(wrapper->get_result_sublattice_conc_2x2(),
                                output_file_name_sublattice_conc_2x2);
-=======
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
                 vector_to_file(wrapper->get_result_wtd_2x2(),
                                output_file_name_wtd_2x2);
                 vector_to_file(wrapper->get_result_correlations_2x2(),
                                output_file_name_corr_2x2);
         }
-<<<<<<< HEAD
         // for(double d : wrapper->get_result_sublattice_conc_2x2()) { std::cout << d << std::endl;}
-=======
->>>>>>> 0bff648d5adb391c294373620f1565d115c1c73c
         printf("\rProgress:  DONE\n");
         // - - - - - - - - - - - - - - - - - - - - - - - - -
 }
